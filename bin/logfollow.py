9f25632f0e512b6bd7c66f99d58e992f9f0ffe81
Alexey S. Kachayev <kachayev@gmail.com>
2011-10-16 23:59:16 +0300

    Fix error with catching two types of exceptions

--- bin/logfollow.py
+++ bin/logfollow.py
@@ -43,7 +43,7 @@ class LogStreamer(object):
         """Remove client from list of followers"""
         try:
             cls.stream[path]['followers'].remove(follower)
-        except KeyError, TypeError:
+        except (KeyError, TypeError):
             pass
 
     @classmethod
