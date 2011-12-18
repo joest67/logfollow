0a5f8694b7bd13a4ac0cad203e8c44e6f35c8c84
Alexey S. Kachayev <kachayev@gmail.com>
2011-12-18 16:32:57 +0200

    Add classifiers in setup script

--- setup.py
+++ setup.py
@@ -57,5 +57,23 @@ setup(
     entry_points = {
         "distutils.commands": 
             ["upload_scripts = logfollow.install:StaticFilesUploader"]
-    }
+    },
+    classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: System :: Software Distribution',
+          'Topic :: System :: Systems Administration',
+    ]
 )
